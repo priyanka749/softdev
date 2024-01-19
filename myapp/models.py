0da0058ff059f25fd3c93122739ac9f52bb6b4ac
Sneha <snehasingh2k3@gmail.com>
2024-01-19 11:16:52 +0545

    feedback

---
+++ myapp/models.py
@@ -0,0 +1,217 @@
+from django.forms import ValidationError
+
+from django.db import models
+from django.db.models import ImageField
+
+class User(models.Model):    
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+from django.db import models
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=10)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# models.py
+from django.db import models
+
+class Animal_dog(models.Model):
+    BREED_CHOICES = [
+
+        ('German Shepherd', 'German Shepherd'),
+        ('Bulldog', 'Bulldog'),
+        ('French Bulldog', 'French Bulldog'),
+        ('Golden Retriever', 'Golden Retriever'),
+        ('Chow Chow', 'Chow Chow'),
+    ]
+
+    COLOR_CHOICES = [
+        ('black', 'Black'),
+        ('brown', 'Brown'),
+        ('white', 'White'),
+        ('grey', 'Grey'),
+        ('red', 'Red'),
+        ('blue', 'Blue'),
+        ('gold', 'Gold'),
+        ('cream', 'Cream'),
+    ]
+
+    BEHAVIOR_CHOICES = [
+        ('Digging', 'Digging'),
+        ('Eating Poop', 'Eating Poop'),
+        ('Head Pressing', 'Head Pressing'),
+        ('Scooting', 'Scooting'),
+        ('Yawning', 'Yawning'),
+        ('Biting', 'Biting'),
+        ('Circling', 'Circling'),
+    ]
+
+    AGE_CHOICES = [
+        (1, 'One'),
+        (2, 'Two'),
+        (3, 'Three'),
+        (4, 'Four'),
+        (5, 'Five'),
+        (6, 'Six'),
+        (7, 'Seven'),
+    ]
+
+    SIZE_CHOICES = [
+        ('10-20 small lbs', '1 Queen Pillow'),
+        ('30-40 lbs medium', '2 Standard Pillows'),
+        ('50-60 lbs large', '2 King Pillows'),
+        ('70-80 lbs xl', '3 King Pillows'),
+    ]
+
+    pet_id = models.AutoField(primary_key=True)
+    
+    # id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,default='pet')
+    breed = models.CharField(max_length=255, default='streetdog', choices=BREED_CHOICES)
+    color = models.CharField(max_length=255, default='kalo',choices=COLOR_CHOICES)
+    behavior = models.CharField(max_length=255,default='default_behavior',choices=BEHAVIOR_CHOICES,)
+    age = models.IntegerField(default='default_behavior', choices=AGE_CHOICES)
+    weight = models.CharField(max_length=255, default='default_behavior', choices=SIZE_CHOICES)
+    image = models.ImageField(upload_to='animal_images/', default='default_image.jpg')
+    def __str__(self):
+        return self.name
+
+class Cats(models.Model):
+
+    COLOR_CHOICES = [
+        ('black', 'Black'),
+        ('brown', 'Brown'),
+        ('white', 'White'),
+        ('grey', 'Grey'),
+        ('tabby', 'tabby'),
+        ('Calico', 'Calico'),
+    ]
+
+    Friendliness_options = [
+        ('Outgoing', 'Outgoing'),
+        ('very social', 'very social'),
+        ('Reserved', 'Reserved'),
+    ]
+
+    AGE_CHOICES = [
+        (1, 'One'),
+        (2, 'Two'),
+        (3, 'Three'),
+        (4, 'Four'),
+        (5, 'Five'),
+        (6, 'Six'),
+        (7, 'Seven'),
+    ]
+
+    SIZE_CHOICES = [
+        ('10-20 small lbs', 'Small'),
+        ('30-40 lbs medium', 'Medium'),
+        ('50-60 lbs large', 'large'),     
+    ]
+
+    cat_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,default='kitty')
+    color = models.CharField(max_length=255, default='Tabby',choices=COLOR_CHOICES)
+    age = models.IntegerField(default='Unknown', choices=AGE_CHOICES)
+    weight = models.CharField(max_length=255, default='default_behavior', choices=SIZE_CHOICES)
+    Friendliness=models.CharField(max_length=255, default='medium', choices=Friendliness_options)
+    image = models.ImageField(upload_to='animal_images/', max_length=250,null=True,default='default_image.jpg')
+    def __str__(self):
+        return self.name
+
+
+from django.db import models
+
+class Birds(models.Model):
+    COLOR_CHOICES = [
+        ('black', 'Black'),
+        ('brown', 'Brown'),
+        ('white', 'White'),
+        ('grey', 'Grey'),
+        ('Tabby', 'Tabby'),
+    ]
+
+    AGE_CHOICES = [
+        (1, 'One'),
+        (2, 'Two'),
+        (3, 'Three'),
+        (4, 'Four'),
+        (5, 'Five'),
+        (6, 'Six'),
+        (7, 'Seven'),
+    ]
+
+    SPECIES_CHOICES = [
+        ('sparrow', 'Sparrow'),
+        ('parrot', 'Parrot'),
+        ('Cockatiel', 'Cockatiel'),
+        ('Lovebird', 'Lovebird'),
+    ]
+
+    SIZE_CHOICES = [
+        ('10-20 small lbs', 'Small'),
+        ('30-40 lbs medium', 'Medium'),
+        ('50-60 lbs large', 'Large'),
+    ]
+
+    FRIENDLINESS_CHOICES = [
+        ('Outgoing', 'Outgoing'),
+        ('very social', 'Very Social'),
+        ('Reserved', 'Reserved'),
+    ]
+
+    bird_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, default='Kitty')
+    color = models.CharField(max_length=255, default='Tabby', choices=COLOR_CHOICES)
+    age = models.IntegerField(default=1, choices=AGE_CHOICES)
+    species = models.CharField(max_length=255, default='parrot', choices=SPECIES_CHOICES)
+    size = models.CharField(max_length=255, default='10-20 small lbs', choices=SIZE_CHOICES)
+    friendliness = models.CharField(max_length=255, default='Outgoing', choices=FRIENDLINESS_CHOICES)
+    image = models.ImageField(upload_to='pet_images/', max_length=250, null=True, default='default_image.jpg')
+
+    def __str__(self):
+        return self.name
+
+
+
+class other_animals(models.Model):
+
+    animal_id=models.AutoField(primary_key=True)
+    animal= models.CharField(max_length=100)
+    age=models.IntegerField(default=1)
+    about_it= models.TextField()
+    image = models.ImageField(upload_to='pet_images/', max_length=250, null=True, default='default_image.jpg')
+
+    def __str__(self):
+        return self.animal
+
+
+class Donation_data(models.Model):
+    donate_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    message = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    image = models.ImageField(upload_to='donation_images/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Donation #{self.donate_id} - {self.full_name}"
+
+
+class FeedBack(models.Model):
+    name=models.CharField( max_length=100)
+    email=models.EmailField()
+    feedback=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
---
+++ myapp/urls.py
@@ -0,0 +1,44 @@
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+from django.urls import path
+from django.views import View
+from .views import profile
+from myapp import views
+from . import views
+
+# from .views import AnimaList
+
+urlpatterns =[
+    path('', views.dash,name='dash'),
+    path('signup/', views.signup, name='signup'),
+    path('login/',views.login,name='login'),
+    path('contact/', views.contact, name='contact'),
+    path('dash/', views.dash, name='dash'),
+    path('faq/',views.faq,name='faq'),
+    path('aboutus/',views.aboutus,name='aboutus'),
+    
+    path('showpets/', views.showpets, name='showpets'),
+
+    path('showpets/<str:animal_type>/<int:pet_id>/', views.profile, name='profile'),
+
+    path('contact/', views.contact_view, name='contact'),
+    path('showcats/',views.showcats, name='showcats'),
+    path('showbirds/',views.showbirds, name='showbirds'),
+
+    path('show_more/',views.SearchView, name='show_more'),
+
+    path('donation_form/',views.save_donateInfo, name='save_donateInfo'),
+    path('feedback/',views.Feedback,name='feedback'),
+    
+   
+]
+
+
+
+# Serving media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
